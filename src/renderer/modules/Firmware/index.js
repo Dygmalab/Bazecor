74dd1db12a89e08dbd24eee773c4d9d59bb4f008
Alejandro Parcet <alex@dygma.com>
2023-06-05 12:36:20 +0200

    Added new firmware folder files
    Signed-off-by: Alejandro Parcet <alex@dygma.com>

---
+++ src/renderer/modules/Firmware/index.js
@@ -0,0 +1,35 @@
+/* Bazecor
+ * Copyright (C) 2023  Dygmalab, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import FirmwareUpdatePanel from "./FirmwareUpdatePanel";
+import FirmwareAdvancedOptions from "./FirmwareAdvancedOptions";
+import FirmwareUpdateProcess from "./FirmwareUpdateProcess";
+import FirmwareNeuronStatus from "./FirmwareNeuronStatus";
+import FirmwareVersionStatus from "./FirmwareVersionStatus";
+import FirmwareProgressStatus from "./FirmwareProgressStatus";
+import FirmwareImageHelp from "./FirmwareImageHelp";
+import FirmwareNeuronHelp from "./FirmwareNeuronHelp";
+
+export {
+  FirmwareUpdatePanel,
+  FirmwareUpdateProcess,
+  FirmwareAdvancedOptions,
+  FirmwareNeuronStatus,
+  FirmwareVersionStatus,
+  FirmwareProgressStatus,
+  FirmwareImageHelp,
+  FirmwareNeuronHelp
+};
