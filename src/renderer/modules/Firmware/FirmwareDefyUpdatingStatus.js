1d30839bb31d41683a92e65a21214c94d1bbc53e
Thiago Hernandez <thiago.hernandez@gmail.com>
2023-06-15 11:30:04 +0200

    Added new component to indicate the Defy updating status

---
+++ src/renderer/modules/Firmware/FirmwareDefyUpdatingStatus.js
@@ -0,0 +1,27 @@
+// -*- mode: js-jsx -*-
+/* Bazecor
+ * Copyright (C) 2022  Dygmalab, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from "react";
+import Styled from "styled-components";
+
+const Style = Styled.div`
+`;
+const FirmwareDefyUpdatingStatus = ({ isUpdated, status, deviceProduct, keyboardType, icon }) => {
+  return <Style>Sides status</Style>;
+};
+
+export default FirmwareDefyUpdatingStatus;
