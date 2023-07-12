464e0140b0ff1095c0b494faf24736635e7da965
Javier Guzman <906599+javierguzman@users.noreply.github.com>
2023-07-12 10:34:13 +0200

    test: initial e2e draft

--- e2e_tests/example.spec.ts
+++ e2e_tests/example.spec.ts
@@ -3,7 +3,6 @@ import { _electron as electron, test } from "@playwright/test";
 test("save screenshot", async () => {
   const electronApp = await electron.launch({ args: [".webpack/main/index.js"], timeout: 300000 });
   const window = await electronApp.firstWindow();
-  await window.screenshot({ path: "intro.png" });
-  // close app
+  await window.screenshot({ path: "landing.png" });
   await electronApp.close();
 });
