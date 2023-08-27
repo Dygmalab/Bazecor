5260374309fd841704478c196c539b9457b6d0db
Johan Eklund <hello@johaneklund.com>
2023-08-27 20:59:33 +0200

    Update documentation to refer to Microsoft and Keybord Layout Info websites for language codes
    Signed-off-by: Johan Eklund <hello@johaneklund.com>

--- src/renderer/modules/Settings/GeneralSettingsLanguages.js
+++ src/renderer/modules/Settings/GeneralSettingsLanguages.js
@@ -1,20 +1,14 @@
 /**
- * Please use The Unicode Consortium Langugage-Territory Information database
- * for finding your unique language and territory code.
- * http://www.unicode.org/cldr/charts/latest/supplemental/language_territory_information.html
+ * You can find a list of keyboard identifiers languages on Microsoft website:
+ * https://learn.microsoft.com/en-us/windows-hardware/manufacture/desktop/default-input-locales-for-windows-language-packs?view=windows-11
  *
- * | Language | Code | Territory   | Code |
- * | -------- | ---- | ----------- | ---- |
- * | German   | de   | Germany {O} | DE   |
- *
- * After you have found your unique language and territory code you will need
- * to combine them. Start with language code (de), add a separator (-) and at
- * last your territory code (DE); "de-DE".
+ * Or by using "Keyboard Layout Info" website, where you should look for KLID.
+ * https://kbdlayout.info
  *
  * In case your keyboard layout aren't an officall language e.g. EurKEY you
  * select the language code that it's based off (en), add a seperator (-) and
- * you create a custom territory code that matches the given name of the
- * layout (EU); "en-EU".
+ * you create a custom code that matches the given name of the layout (EU);
+ * "en-EU"
  *
  * All flags MUST be a circle with the dimensions of 512x512 pixels, you can
  * find all offical language flags on https://flagpedia.net/download/vector.
