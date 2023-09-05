bb550f6b1c1cb451cc28a48b6c895842d62987fa
Javier Guzman <906599+javierguzman@users.noreply.github.com>
2023-09-05 15:44:24 +0200

    feat: can connect to HID ok

---
+++ src/main/webapi/hid.ts
@@ -0,0 +1,43 @@
+import Window from "../managers/Window";
+
+export const addHIDListeners = () => {};
+
+export const removeHIDListeners = () => {};
+
+export const configureHID = () => {
+  const window = Window.getWindow();
+
+  window.webContents.session.setPermissionCheckHandler((webContents, permission, requestingOrigin, details) => {
+    if (permission === "hid" && details.securityOrigin.includes("http://localhost")) {
+      return true;
+    }
+    return false;
+  });
+
+  window.webContents.session.setDevicePermissionHandler(details => {
+    if (details.deviceType === "hid" && details.origin.includes("http://localhost")) {
+      return true;
+    }
+    return false;
+  });
+  window.webContents.session.on("select-hid-device", (event, details, callback) => {
+    // Add events to handle devices being added or removed before the callback on
+    // `select-hid-device` is called.
+    event.preventDefault();
+    window.webContents.session.on("hid-device-added", (added_device_event, device) => {
+      console.log("hid-device-added FIRED WITH", device);
+      // Optionally update details.deviceList
+    });
+
+    window.webContents.session.on("hid-device-removed", (removed_device_event, device) => {
+      console.log("hid-device-removed FIRED WITH", device);
+    });
+    if (details.deviceList && details.deviceList.length > 0) {
+      const filteredDevices = details.deviceList.filter(device => device.name.includes("ERGO"));
+      console.log(filteredDevices);
+      if (filteredDevices.length > 0) {
+        callback(filteredDevices[0].deviceId);
+      }
+    }
+  });
+};
--- src/renderer/component/TestArea.tsx
+++ src/renderer/component/TestArea.tsx
@@ -4,12 +4,31 @@ import { RegularButton } from "./Button";
 const TestArea = () => {
   const testingMode = true;
 
-  const onGetHIDDevices = () => {
-    console.log("Getting HID devices");
+  const onGetHIDDevices = async () => {
+    const grantedDevices = await navigator.hid.getDevices();
+    console.log(grantedDevices);
+  };
+
+  const onHIDConnect = async () => {
+    const device = await navigator.hid.requestDevice({
+      filters: [
+        {
+          vendorId: 1133,
+          productId: 45095,
+        },
+      ],
+    });
+    console.log("Connected to");
+    console.log(device);
   };
 
   if (testingMode) {
-    return <RegularButton buttonText="List of HID Devices" styles="primary" onClick={onGetHIDDevices} />;
+    return (
+      <>
+        <RegularButton buttonText="List of HID Devices" styles="primary" onClick={onGetHIDDevices} />
+        <RegularButton buttonText="Connect by HID" styles="primary" onClick={onHIDConnect} />
+      </>
+    );
   }
   return <></>;
 };
