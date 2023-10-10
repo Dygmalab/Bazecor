74fdc301b3fd77adf197e9f9926000aeb47b76d9
AlexDygma <alex@dygma.com>
2023-10-10 14:09:56 +0200

    added ts-nocheck to required files

--- src/api/hid/hid.ts
+++ src/api/hid/hid.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import Hardware from "../hardware";
 
 const DygmavendorID = 13807;
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 // -*- mode: js-jsx -*-
 /* Bazecor -- Kaleidoscope Command Center
  * Copyright (C) 2018, 2019  Keyboardio, Inc.
