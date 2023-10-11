24533bd908d2627e4ea65733d041f5ee658067c4
Alejandro Parcet <alex@dygma.com>
2023-10-11 15:46:20 +0200

    fix: adding license and ts-nocheck to Devtools.ts file

--- src/renderer/views/BazecorDevtools.tsx
+++ src/renderer/views/BazecorDevtools.tsx
@@ -1,3 +1,22 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+// -*- mode: js-jsx -*-
+/* Bazecor -- Kaleidoscope Command Center
+ * Copyright (C) 2023  DygmaLab SE
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import React, { useState } from "react";
 import Styled from "styled-components";
 import Container from "react-bootstrap/Container";
