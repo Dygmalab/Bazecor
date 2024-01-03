0d568aa8ff4353b36c6a7a1e585c8af0b2ce2575
Thiago Hernandez <thiago.hernandez@gmail.com>
2024-01-03 18:31:30 +0100

    Update dependencies on NameModal  component

--- src/renderer/component/Modal/NameModal.tsx
+++ src/renderer/component/Modal/NameModal.tsx
@@ -1,32 +1,78 @@
-import React, { useState, useRef } from "react";
-import { Dialog } from "@headlessui/react";
-import { AnimatePresence, motion } from "framer-motion";
+import React, { useState, useEffect, useRef, Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 import i18n from "../../i18n";
 
-const NameModal = ({ modalTitle, show, toggleShow, name, handleSave, label }) => {
+interface NameModalProps {
+  modalTitle: string;
+  show: boolean;
+  name: string;
+  toggleShow: () => void;
+  handleSave: (event) => void;
+  labelInput: string;
+}
+
+const NameModal = ({ modalTitle, show, toggleShow, name, handleSave, labelInput }: NameModalProps) => {
   const [internalName, setInternalName] = useState(name);
+  const [isFocused, setIsFocused] = useState(false);
   const inputRef = useRef(null);
+
+  useEffect(() => {
+    const handleKeydown = (event: any) => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        if (inputRef.current && isFocused) {
+          handleSave(internalName);
+          toggleShow();
+        }
+      }
+    };
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  }, []);
 
   return (
     // Use the `Transition` component + show prop to add transitions.
-    <AnimatePresence>
-      {open && (
-        <Dialog static as={motion.div} open={show} onClose={toggleShow} initialFocus={inputRef} className="relative z-50">
-          {/* The backdrop, rendered as a fixed sibling to the panel container */}
-          <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+    <Transition show={show} as={Fragment}>
+      <Dialog onClose={toggleShow} initialFocus={inputRef} className="relative z-50">
+        {/* The backdrop, rendered as a fixed sibling to the panel container */}
 
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+        </Transition.Child>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
           {/* Full-screen container to center the panel */}
           <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
             {/* The actual dialog panel  */}
             <Dialog.Panel className="mx-auto max-w-sm rounded bg-white">
               <Dialog.Title>{modalTitle}</Dialog.Title>
               <form>
-                <label htmlFor="changeName">{label}</label>
+                <label htmlFor="changeName">{labelInput}</label>
                 <input
                   id="changeName"
                   type="text"
                   ref={inputRef}
                   value={internalName}
+                  onFocus={() => setIsFocused(true)}
+                  onBlur={() => setIsFocused(false)}
                   onChange={event => setInternalName(event.target.value)}
                 />
                 <button onClick={toggleShow} className="btn btn-outline" type="button">
@@ -38,9 +84,9 @@ const NameModal = ({ modalTitle, show, toggleShow, name, handleSave, label }) =>
               </form>
             </Dialog.Panel>
           </div>
-        </Dialog>
-      )}
-    </AnimatePresence>
+        </Transition.Child>
+      </Dialog>
+    </Transition>
   );
 };
 
