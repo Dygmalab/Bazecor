3efc200748e08449ca18e9a8a1250270300e7c27
unional <homawong@gmail.com>
2024-01-10 22:09:47 -0800

    refactor: fixing some obvious typos in styles

--- src/renderer/modules/Firmware/FirmwareCheckProcessPanel.jsx
+++ src/renderer/modules/Firmware/FirmwareCheckProcessPanel.jsx
@@ -98,7 +98,7 @@ height:inherit;
   z-index: 9;
 
   .buttonToggler.dropdown-toggle.btn {
-    color: ${({ theme }) => theme.styles.firmwareUpdatePanel.iconDropodownColor};
+    color: ${({ theme }) => theme.styles.firmwareUpdatePanel.iconDropdownColor};
   }
 }
 .wrapperActions {
