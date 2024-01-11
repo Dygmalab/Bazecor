48061e3bd6dc667e553569ddd15dee253165b057
unional <homawong@gmail.com>
2024-01-10 22:44:24 -0800

    refactor: convert some code to TS
    The conversion will be done in many small PRs,
    so that the changes can be merged quickly,
    minimizing merge conflicts for the team.

--- src/api/color/RGBWtoRGB.ts
+++ src/api/color/RGBWtoRGB.ts
@@ -1,4 +1,11 @@
-export default function rgbw2b(rgbw) {
+type RGBW = {
+  r: number,
+  g: number,
+  b: number,
+  w: number
+}
+
+export default function rgbw2b(rgbw: RGBW) {
   const r = rgbw.w + rgbw.r > 255 ? 255 : rgbw.w + rgbw.r;
   const g = rgbw.w + rgbw.g > 255 ? 255 : rgbw.w + rgbw.g;
   const b = rgbw.w + rgbw.b > 255 ? 255 : rgbw.w + rgbw.b;
