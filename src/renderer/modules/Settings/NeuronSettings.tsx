8dd706c8e0f8c73df43f8540dab6b1abe1ca4dee
Alejandro Parcet Gonzalez <alexpargon@gmail.com>
2024-01-16 10:36:52 +0100

    refactor: added types for independent neuron and changed NeuronSettings to FC+types

--- src/renderer/modules/Settings/NeuronSettings.tsx
+++ src/renderer/modules/Settings/NeuronSettings.tsx
@@ -1,63 +1,60 @@
-import PropTypes from "prop-types";
-import React, { Component } from "react";
+import React from "react";
 
 // React Bootstrap Components
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import i18n from "../../i18n";
+import i18n from "@Renderer/i18n";
 
 // Own Components
-import Title from "../../component/Title";
-import { NeuronSelector } from "../../component/Select";
-import NeuronData from "../NeuronData";
+import Title from "@Renderer/component/Title";
+import { NeuronSelector } from "@Renderer/component/Select";
+import NeuronData from "@Renderer/modules/NeuronData";
+import { Neuron } from "@Types/neurons";
 
 // Icons Imports
-import { IconNeuronManager } from "../../component/Icon";
+import { IconNeuronManager } from "@Renderer/component/Icon";
 
-export default class NeuronSettings extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { neurons, selectedNeuron, selectNeuron, updateNeuronName, deleteNeuron } = this.props;
-    return (
-      <Card className="overflowFix card-preferences mt-4">
-        <Card.Title>
-          <Title text={i18n.keyboardSettings.neuronManager.header} headingLevel={3} svgICO={<IconNeuronManager />} />
-        </Card.Title>
-        <Card.Body className="py-0">
-          <Form.Group controlId="backupFolder" className="mb-3">
-            <NeuronSelector
-              onSelect={selectNeuron}
-              itemList={neurons}
-              selectedItem={selectedNeuron}
-              updateItem={updateNeuronName}
-              deleteItem={deleteNeuron}
-              subtitle={i18n.keyboardSettings.neuronManager.neuronLabel}
-            />
-            <Row className="mb-4 mt-4">
-              <Col>
-                {Array.isArray(neurons) && neurons.length > 0 ? (
-                  <NeuronData neurons={neurons} selectedNeuron={selectedNeuron} />
-                ) : (
-                  ""
-                )}
-              </Col>
-            </Row>
-          </Form.Group>
-        </Card.Body>
-      </Card>
-    );
-  }
+interface NeuronSettingsProps {
+  neurons: [Neuron];
+  selectedNeuron: number;
+  selectNeuron: () => void;
+  updateNeuronName: () => void;
+  deleteNeuron: () => void;
 }
 
-NeuronSettings.propTypes = {
-  neurons: PropTypes.array.isRequired,
-  selectedNeuron: PropTypes.number.isRequired,
-  selectNeuron: PropTypes.func.isRequired,
-  updateNeuronName: PropTypes.func.isRequired,
-  deleteNeuron: PropTypes.func.isRequired,
-};
+function NeuronSettings(props: NeuronSettingsProps) {
+  const { neurons, selectedNeuron, selectNeuron, updateNeuronName, deleteNeuron } = props;
+
+  return (
+    <Card className="overflowFix card-preferences mt-4">
+      <Card.Title>
+        <Title text={i18n.keyboardSettings.neuronManager.header} headingLevel={3} svgICO={<IconNeuronManager />} />
+      </Card.Title>
+      <Card.Body className="py-0">
+        <Form.Group controlId="backupFolder" className="mb-3">
+          <NeuronSelector
+            onSelect={selectNeuron}
+            itemList={neurons}
+            selectedItem={selectedNeuron}
+            updateItem={updateNeuronName}
+            deleteItem={deleteNeuron}
+            subtitle={i18n.keyboardSettings.neuronManager.neuronLabel}
+          />
+          <Row className="mb-4 mt-4">
+            <Col>
+              {Array.isArray(neurons) && neurons.length > 0 ? (
+                <NeuronData neurons={neurons} selectedNeuron={selectedNeuron} />
+              ) : (
+                ""
+              )}
+            </Col>
+          </Row>
+        </Form.Group>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default NeuronSettings;
--- src/renderer/types/neurons.ts
+++ src/renderer/types/neurons.ts
@@ -1,30 +1,30 @@
-export interface Neurons {
-  neurons: [
+export interface Neuron {
+  id: string;
+  name: string;
+  layers: [
     {
-      id: string;
+      id: number;
       name: string;
-      layers: [
-        {
-          id: number;
-          name: string;
-        },
-      ];
-      macros: [
-        {
-          actions: unknown;
-          id: number;
-          name: string;
-          macro: string;
-        },
-      ];
-      superkeys: [
-        {
-          actions: unknown;
-          name: string;
-          id: number;
-        },
-      ];
     },
   ];
+  macros: [
+    {
+      actions: unknown;
+      id: number;
+      name: string;
+      macro: string;
+    },
+  ];
+  superkeys: [
+    {
+      actions: unknown;
+      name: string;
+      id: number;
+    },
+  ];
+}
+
+export interface Neurons {
+  neurons: [Neuron];
   selectedNeuron: number;
 }
