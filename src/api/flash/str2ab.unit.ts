ca1fd2b2c331ddcbfbc2bc05bee4e911f7803bbd
unional <homawong@gmail.com>
2024-01-27 00:37:38 -0800

    test: convert ascii and trim

--- src/api/flash/str2ab.unit.ts
+++ src/api/flash/str2ab.unit.ts
@@ -10,3 +10,17 @@ it('returns an empty array buffer when the input string is empty', () => {
 	const r = str2ab('')
 	expect(r.byteLength).toBe(0)
 })
+
+it('converts ASCII to byte array', () => {
+	const r = str2ab('ABC')
+	expect(r.byteLength).toBe(3)
+	const dec = new TextDecoder()
+  expect(dec.decode(r)).toEqual('ABC')
+})
+
+// TODO: this is the current behavior, not sure if this causes any limitations.ts
+it('trims unicode to 2 bytes', () => {
+	const r = str2ab('あ')
+	const dec = new TextDecoder()
+  expect(dec.decode(r)).not.toEqual('あ')
+})
