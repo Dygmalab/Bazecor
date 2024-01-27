cfe2ad58601c70f23bd36305c35982708aea4b1c
unional <homawong@gmail.com>
2024-01-27 01:52:14 -0800

    refactor: padToN signature
    the `num2hexstr` usage actually shows that the first param is a string,
    and the second param is opional based on the commented console log.
    
    the value is defaults to 0 to better match its intent and passes type check.

--- src/api/flash/padToN.ts
+++ src/api/flash/padToN.ts
@@ -1,4 +1,4 @@
-export function padToN(number: number, numberToPad: number) {
+export function padToN(number: string, numberToPad: number = 0) {
   let str = "";
 
   for (let i = 0; i < numberToPad; i++) str += "0";
