78b776934fab95ed698b62910a6b4d37a61b2101
unional <homawong@gmail.com>
2024-01-27 00:08:22 -0800

    test: zero length

--- src/api/flash/str2ab.unit.ts
+++ src/api/flash/str2ab.unit.ts
@@ -5,3 +5,8 @@ it('converts input string to an ArrayBuffer', () => {
 	const r = str2ab('')
 	expect(r).instanceOf(ArrayBuffer)
 })
+
+it('returns an empty array buffer when the input string is empty', () => {
+	const r = str2ab('')
+	expect(r.byteLength).toBe(0)
+})
