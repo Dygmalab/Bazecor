f34d9b2aa23ee069fb301e55d4c68423d1357b53
Alejandro Parcet Gonzalez <alexpargon@gmail.com>
2024-02-13 17:33:06 +0100

    fix: called Raise2 definition types

--- src/api/hardware-virtual/index.ts
+++ src/api/hardware-virtual/index.ts
@@ -2,7 +2,9 @@ import RaiseANSI from "./RaiseANSI";
 import RaiseISO from "./RaiseISO";
 import DefyWired from "./DefyWired";
 import DefyWireless from "./DefyWireless";
+import Raise2ANSI from "./Raise2ANSI";
+import Raise2ISO from "./Raise2ISO";
 
-const enumerator = [RaiseANSI, RaiseISO, DefyWireless];
+const enumerator = [RaiseANSI, RaiseISO, DefyWireless, Raise2ANSI, Raise2ISO];
 
-export { RaiseISO, RaiseANSI, DefyWired, DefyWireless, enumerator };
+export { RaiseISO, RaiseANSI, DefyWired, DefyWireless, Raise2ANSI, Raise2ISO, enumerator };
