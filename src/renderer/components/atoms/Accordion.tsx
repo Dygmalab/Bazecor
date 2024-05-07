0490a3f96c93b45e43749a50f660d77acb7a7ef1
Thiago Hernandez <thiago.hernandez@gmail.com>
2024-05-07 13:08:40 +0200

    fix: fix padding on accordion placed on SuperKeys view

--- src/renderer/components/atoms/Accordion.tsx
+++ src/renderer/components/atoms/Accordion.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import * as AccordionPrimitive from "@radix-ui/react-accordion";
-import { cva, type VariantProps } from "class-variance-authority";
+// import { cva, type VariantProps } from "class-variance-authority";
 
 import { ChevronDown } from "lucide-react";
 
--- src/renderer/modules/Superkeys/SuperKeysFeatures.tsx
+++ src/renderer/modules/Superkeys/SuperKeysFeatures.tsx
@@ -72,7 +72,7 @@ function SuperKeysFeatures() {
     <Style>
       <Accordion type="single" collapsible className="w-full rounded bg-gray-25/80 dark:bg-gray-600 mt-3">
         <AccordionItem value="item-1" className="border-0">
-          <AccordionTrigger className="flex justify-between items-center py-3 mt-0 mb-[-1px] rounded-none text-purple-200 dark:text-gray-25 border-b border-solid border-gray-50 dark:border-gray-600">
+          <AccordionTrigger className="flex justify-between items-center py-3 px-3 mt-0 mb-[-1px] rounded-none text-purple-200 dark:text-gray-25 border-b border-solid border-gray-50 dark:border-gray-600">
             <strong className="text-xs tracking-tight font-semibold">{i18n.editor.superkeys.collapse.title}</strong>
           </AccordionTrigger>
           <AccordionContent className="px-3 py-3 bg-transparent border-0">
