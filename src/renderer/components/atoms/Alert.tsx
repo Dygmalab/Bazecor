61d557d3688b0829c8c8d428a1ac2e7dbc829df2
Thiago Hernandez <thiago.hernandez@gmail.com>
2024-05-10 15:31:30 +0200

    chore: added styles and variations to the new callout component

--- src/renderer/components/atoms/Alert.tsx
+++ src/renderer/components/atoms/Alert.tsx
@@ -3,31 +3,45 @@ import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@Renderer/utils";
 
-const alertVariants = cva(
-  "relative w-full rounded-lg border border-slate-200 p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-slate-950 dark:border-slate-800 dark:[&>svg]:text-slate-50",
-  {
-    variants: {
-      variant: {
-        default:
-          "bg-[#ffffff]/10 dark:bg-transparent text-gray-400 dark:text-gray-25 rounded font-normal leading-2 border-[1px] border-solid border-gray-100/70 dark:border-gray-600/70",
-        destructive:
-          "border-red-500/50 text-red-500 dark:border-red-500 [&>svg]:text-red-500 dark:border-red-900/50 dark:text-red-900 dark:dark:border-red-900 dark:[&>svg]:text-red-900",
-      },
-      size: {
-        sm: "text-xs px-6 py-4 [&>svg]:",
-        md: "text-sm",
-        lg: "text-base",
-      },
+const alertVariants = cva("relative w-full rounded-lg", {
+  variants: {
+    variant: {
+      default:
+        "bg-[#ffffff]/10 dark:bg-transparent text-gray-400 dark:text-gray-25 rounded font-normal leading-2 border-[1px] border-solid border-gray-100/70 dark:border-gray-600/70",
+      destructive:
+        "border-red-500/50 text-red-500 dark:border-red-500 [&>svg]:text-red-500 dark:border-red-900/50 dark:text-red-900 dark:dark:border-red-900 dark:[&>svg]:text-red-900",
     },
-    defaultVariants: {
-      variant: "default",
+    behaviour: {
+      default:
+        "[&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:text-slate-950 dark:border-slate-800 dark:[&>svg]:text-slate-50",
+      callout: "flex [&>svg]:absolute [&_svg]:left-[-18px] [&_svg]:top-4",
+    },
+    size: {
+      sm: "text-xs px-[24px] py-[16px]",
+      md: "text-sm",
+      lg: "text-base",
     },
   },
-);
+  compoundVariants: [
+    // Applied via:
+    //   `button({ intent: "primary", size: "medium" })`
+    {
+      variant: "default",
+      behaviour: "callout",
+      size: "sm",
+      className: "[&_svg]:top-[10px]",
+    },
+  ],
+  defaultVariants: {
+    variant: "default",
+    behaviour: "default",
+    size: "md",
+  },
+});
 
 const Alert = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>>(
-  ({ className, variant, size, ...props }, ref) => (
-    <div ref={ref} role="alert" className={cn(alertVariants({ variant }), className)} {...props} />
+  ({ className, variant, size, behaviour, ...props }, ref) => (
+    <div ref={ref} role="alert" className={cn(alertVariants({ variant, size, behaviour, className }))} {...props} />
   ),
 );
 Alert.displayName = "Alert";
@@ -40,7 +54,9 @@ const AlertTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<H
 AlertTitle.displayName = "AlertTitle";
 
 const AlertDescription = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLParagraphElement>>(
-  ({ className, ...props }, ref) => <div ref={ref} className={cn("text-sm [&_p]:leading-relaxed", className)} {...props} />,
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn("text-sm [&_p]:leading-relaxed [&_p:last-of-type]:mb-0", className)} {...props} />
+  ),
 );
 AlertDescription.displayName = "AlertDescription";
 
--- src/renderer/views/MacroEditor.tsx
+++ src/renderer/views/MacroEditor.tsx
@@ -50,7 +50,7 @@ import Callout from "@Renderer/component/Callout";
 import { IconFloppyDisk, IconLoader } from "@Renderer/component/Icon";
 import { MacroSelector } from "@Renderer/component/Select";
 import ToastMessage from "@Renderer/component/ToastMessage";
-import { Alert, AlertDescription, AlertTitle } from "@Renderer/components/atoms/Alert";
+// import Callout from "@Renderer/components/molecules/Callout/Callout";
 
 // Modules
 import { PageHeader } from "@Renderer/modules/PageHeader";
@@ -819,19 +819,17 @@ function MacroEditor(props: MacroEditorProps) {
           destroyContext={destroyThisContext}
           inContext={modified}
         />
-        <Alert>
-          <svg className="callOutIcon" width="36" height="37" viewBox="0 0 36 37" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="19" cy="20" r="17" fill="currentColor" className="infoShadow" />
-            <circle cx="17" cy="17" r="16.5" fill="currentColor" stroke="currentColor" className="infoCircle" />
-            <path
-              d="M17.186 10.688C17.21 10.172 17.396 9.776 17.744 9.5C18.104 9.224 18.53 9.086 19.022 9.086C19.466 9.086 19.802 9.194 20.03 9.41C20.27 9.614 20.378 9.902 20.354 10.274C20.33 10.742 20.138 11.126 19.778 11.426C19.418 11.714 18.986 11.858 18.482 11.858C18.062 11.858 17.732 11.756 17.492 11.552C17.264 11.336 17.162 11.048 17.186 10.688ZM16.502 15.224C16.706 14.624 16.64 14.324 16.304 14.324C16.088 14.324 15.89 14.432 15.71 14.648C15.53 14.864 15.344 15.23 15.152 15.746L14.792 16.664H14.45L14.954 15.296C15.122 14.816 15.332 14.444 15.584 14.18C15.848 13.916 16.136 13.73 16.448 13.622C16.772 13.502 17.108 13.442 17.456 13.442C17.876 13.442 18.206 13.52 18.446 13.676C18.686 13.832 18.848 14.036 18.932 14.288C19.016 14.528 19.046 14.798 19.022 15.098C18.998 15.386 18.938 15.674 18.842 15.962L16.952 21.47C16.844 21.794 16.814 22.028 16.862 22.172C16.922 22.304 17.036 22.37 17.204 22.37C17.36 22.37 17.528 22.28 17.708 22.1C17.9 21.92 18.11 21.536 18.338 20.948L18.662 20.084H19.004L18.536 21.398C18.368 21.89 18.152 22.268 17.888 22.532C17.624 22.796 17.336 22.982 17.024 23.09C16.724 23.198 16.418 23.252 16.106 23.252C15.59 23.252 15.194 23.138 14.918 22.91C14.654 22.682 14.51 22.352 14.486 21.92C14.462 21.488 14.546 20.978 14.738 20.39L16.502 15.224Z"
-              fill="currentColor"
-              className="iconInfo"
-            />
-          </svg>
-          <AlertTitle>Heads up!</AlertTitle>
-          <AlertDescription>You can add components and dependencies to your app using the cli.</AlertDescription>
-        </Alert>
+
+        {/* <Callout
+          size="sm"
+          className="mt-4"
+          hasVideo
+          media="MfTUvFrHLsE"
+          videoTitle="13 Time-saving MACROS For Your Keyboard"
+          videoDuration="5:24"
+        >
+          {i18n.editor.macros.callout}
+        </Callout> */}
 
         <Callout
           content={i18n.editor.macros.callout}
