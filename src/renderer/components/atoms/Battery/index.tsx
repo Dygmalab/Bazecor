02b414cb53f46323604ab27e855e13665d1274c1
Thiago Hernandez <thiago.hernandez@gmail.com>
2024-05-22 16:05:55 +0200

    refactor: added new battery component to the folder

--- src/renderer/components/atoms/Battery/index.tsx
+++ src/renderer/components/atoms/Battery/index.tsx
@@ -16,16 +16,16 @@
 
 // import BatteryStatusSide from "./BatteryStatusSide";
 import DefyBatteryIndicator from "./DefyBatteryIndicator";
-// import DefyBatteryIndicatorLeft from "./DefyBatteryIndicatorLeft";
-// import DefyBatteryIndicatorRight from "./DefyBatteryIndicatorRight";
+import DefyBatteryIndicatorLeft from "./DefyBatteryIndicatorLeft";
+import DefyBatteryIndicatorRight from "./DefyBatteryIndicatorRight";
 import PileIndicator from "./PileIndicator";
 import SavingModeIndicator from "./SavingModeIndicator";
 
 export {
   // BatteryStatusSide,
   DefyBatteryIndicator,
-  // DefyBatteryIndicatorLeft,
-  // DefyBatteryIndicatorRight,
+  DefyBatteryIndicatorLeft,
+  DefyBatteryIndicatorRight,
   PileIndicator,
   SavingModeIndicator,
 };
