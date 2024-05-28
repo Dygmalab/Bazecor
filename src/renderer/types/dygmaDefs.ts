98b515e928a0075f9b06164a063b772a17e971d8
Alejandro Parcet Gonzalez <alexpargon@gmail.com>
2024-05-28 16:19:48 +0200

    fix: added slider component and dygma definitions

---
+++ src/renderer/types/dygmaDefs.ts
@@ -0,0 +1,38 @@
+export type DygmaDeviceType = {
+  info: {
+    vendor: string;
+    product: string;
+    keyboardType: string;
+    displayName: string;
+    urls: {
+      name: string;
+      url: string;
+    }[];
+  };
+  usb: {
+    vendorId: number;
+    productId: number;
+  };
+  keyboard: {
+    rows: number;
+    columns: number;
+  };
+  keyboardUnderglow: {
+    rows: number;
+    columns: number;
+  };
+  RGBWMode: boolean;
+  components: {
+    keymap: unknown;
+  };
+  instructions: {
+    en: {
+      updateInstructions: string;
+    };
+  };
+  wireless?: boolean;
+  bootloader?: boolean;
+  path?: string;
+  filePath?: string;
+  isDeviceSupported?: (device: DygmaDeviceType) => string;
+};
