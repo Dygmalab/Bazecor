daa3ee37634f28a7fdcb64ee2fd7f617cddbb62e
Thiago Hernandez <thiago.hernandez@gmail.com>
2024-06-11 13:50:04 +0200

    Refactor: Removed the bootstrap dependencies on SelectF13Keys component

--- src/renderer/components/molecules/CustomSelect/SelectF13PlusKeys.tsx
+++ src/renderer/components/molecules/CustomSelect/SelectF13PlusKeys.tsx
@@ -16,54 +16,9 @@
  */
 
 import React from "react";
-import Styled from "styled-components";
-import Dropdown from "react-bootstrap/Dropdown";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@Renderer/components/atoms/select";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectFixedValue } from "@Renderer/components/atoms/select";
 
 import { KeymapDB } from "../../../../api/keymap";
-
-const Style = Styled.div`
-.custom-dropdown {
-  .dropdownItem {
-    padding-left: 0;
-    flex: 0 0 100%;
-    text-align: left;
-    text-transform: capitalize;
-  }
-  .dropdownItemSelected {
-    color: ${({ theme }) => theme.styles.dropdown.textButtonColor};
-    position: relative;
-    .badge-circle{
-        width: 8px;
-        height: 8px;
-        border-radius: 50%;
-        background-color: rgba(254,0,124,1);
-        position: absolute;
-        right: -2px;
-        top: -4px;
-        font-size: 11px;
-    }
-    &:hover {
-      color: ${({ theme }) => theme.styles.dropdown.textButtonHover};
-    }
-  }
-}
-.dropdown-Fkeys {
-    .dropdown-toggle.btn.btn-primary {
-        padding: 0;
-        border: none;
-        margin: 0;
-
-    }
-    .dropdown-toggle::after {
-        content: none;
-    }
-}
-.disabled {
-  pointer-events: none;
-  opacity: 35%;
-}
-`;
 
 function SelectF13PlusKeys(props: any) {
   const { keyCode, onSelect, value, listElements, content, disabled } = props;
@@ -84,10 +39,18 @@ function SelectF13PlusKeys(props: any) {
 
   if (load) return null;
   return (
-    <Style>
+    <div>
       <Select onValueChange={val => onSelect(parseInt(val, 10))} disabled={disabled}>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder={content.first} />
+        <SelectTrigger
+          variant="comboButton"
+          size="sm"
+          className={`w-full pr-[4px] ${
+            keyCode.base > 0 && listElements.map(i => i).includes(keyCode.base)
+              ? "!bg-configButtonActive dark:!bg-configButtonDarkActive bg-purple-200 dark:!bg-purple-300 !border-purple-200 dark:border-none text-white !shadow-buttonConfigLightActive !text-white [&_svg]:!text-white relative after:absolute after:top-[-4px] after:right-[-2px] after:w-[8px] after:h-[8px] after:rounded-full after:bg-primary/100"
+              : ""
+          } ${disabled ? "!pointer-events-none !opacity-50" : ""}`}
+        >
+          <SelectFixedValue label="F13+" />
         </SelectTrigger>
         <SelectContent>
           {listElements.map((item: any, index: number) => (
@@ -98,46 +61,7 @@ function SelectF13PlusKeys(props: any) {
           ))}
         </SelectContent>
       </Select>
-      <Dropdown
-        onSelect={val => onSelect(parseInt(val, 10))}
-        drop="down"
-        flip={false}
-        className={`custom-dropdown dropdown-Fkeys ${disabled ? "disabled" : ""}`}
-      >
-        <Dropdown.Toggle id="dropdown-Fkeys">
-          <div className="dropdownItemSelected">
-            <svg width={65} height={26}>
-              <g filter="url(#filter0_d_2211_181319)">
-                <rect x={0} y={0} width={65} height={26} rx="5" className="baseKey baseKeyDropdown" />
-              </g>
-              <rect x={0} y={0} width={65} height={26} rx="5" fill="url(#paint_gradient)" fillOpacity={0.1} />
-              <g width="12" height="12" fill="transparent">
-                <path d="M1.5 3.5L6 8L10.5 3.5" stroke="currentColor" strokeWidth="2" transform={`translate(${44}, ${6})`} />
-              </g>
-              <text x={22} y={16} fontSize={13} fill="white" fontWeight={600} textAnchor="middle">
-                {content.first}
-              </text>
-            </svg>
-            <div className="badge-circle" />
-          </div>
-        </Dropdown.Toggle>
-        <Dropdown.Menu flip={false}>
-          {listElements.map((item: any, index: number) => (
-            <Dropdown.Item
-              eventKey={item}
-              // eslint-disable-next-line react/no-array-index-key
-              key={`f13Plus-${index}`}
-              className={`${keyCode.base && keyCode.base === item ? "active" : ""}`}
-              pointerEvents="all"
-            >
-              <div className="dropdownInner">
-                <div className="dropdownItem">{labelKey(item)}</div>
-              </div>
-            </Dropdown.Item>
-          ))}
-        </Dropdown.Menu>
-      </Dropdown>
-    </Style>
+    </div>
   );
 }
 
