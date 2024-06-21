e11a420760e19b2e796f8f478e41cf884c1bb069
Fabián Olivares Ríos <f.olivaresrios@gmail.com>
2024-06-20 21:02:19 -0400

    fix: DeviceManager View
    Signed-off-by: Fabián Olivares Ríos <f.olivaresrios@gmail.com>

--- src/renderer/modules/DeviceManager/CardAddDevice.tsx
+++ src/renderer/modules/DeviceManager/CardAddDevice.tsx
@@ -16,22 +16,26 @@ const CardAddDevice = React.forwardRef<HTMLDivElement, CardAddDeviceProps>(({ ad
     ref={ref}
     className="card-device card-filter-on card-all select-none flex flex-col h-full relative p-0 rounded-[24px] border-2 border-solid border-gray-50 bg-gray-25/50 dark:border-gray-700 dark:bg-gray-700/50 overflow-hidden"
   >
-    <div className="card-header relative bg-transparent border-none pt-6 px-6 min-h-[140px]">
-      <Heading headingLevel={3} renderAs="h3">
-        Add keyboard
-      </Heading>
-      <p className="text-gray-600 dark:text-gray-300 text-sm font-semibold tracking-tight">Start setting up your new keyboard</p>
-      <div className="card-alert mt-3 mb-3 flex gap-2">
-        <div className="text-gray-500 dark:text-gray-100 flex-shrink-0 flex-grow-0 basis-[24px]">
-          <IconBluetooth />
-        </div>
-        <p className="w-full text-xs text-gray-500 dark:text-gray-100">
-          <strong>To use Bazecor on bluetooth</strong>, make sure the keyboard is connected via BT to the computer and{" "}
-          <strong> click on scan keyboards once.</strong>
+    <div className="card-header relative bg-transparent border-none pt-6 px-6 min-h-min h-full flex flex-col justify-between gap-4">
+      <div className="card-header-wrapper flex flex-col">
+        <Heading headingLevel={3} renderAs="h3">
+          Add keyboard
+        </Heading>
+        <p className="text-gray-600 dark:text-gray-300 text-base font-semibold tracking-tight pt-2 pb-4">
+          Start setting up your new keyboard
         </p>
+        <div className="card-alert mt-3 mb-3 flex gap-2 border border-amber-300 p-3 rounded-md bg-amber-300/10 items-center">
+          <div className="text-gray-500 dark:text-gray-100 flex-shrink-0 flex-grow-0 basis-[24px] rounded-full p-2 bg-amber-300/50">
+            <IconBluetooth />
+          </div>
+          <p className="w-full text-sm text-gray-500 dark:text-gray-100 font-light">
+            <strong>To use Bazecor on bluetooth</strong>, make sure the keyboard is connected via BT to the computer and{" "}
+            <strong> click on scan keyboards once.</strong>
+          </p>
+        </div>
       </div>
-      <div className="devices-buttons-group flex flex-col items-center justify-center gap-2 mt-6 pb-6 [&_button]:min-w-full">
-        <Button variant="supportive" size="md" icon={<IconRefresh />} onClick={scanDevices}>
+      <div className="devices-buttons-group flex flex-col-reverse sm:flex-row-reverse items-center justify-center gap-2 mt-6 pb-6 [&_button]:min-w-min">
+        <Button variant="primaryIcon" size="md" icon={<IconRefresh />} onClick={scanDevices}>
           <div className="button-content">
             <Heading headingLevel={4} className="text-[1.125rem]">
               {i18n.deviceManager.scanDevices}
