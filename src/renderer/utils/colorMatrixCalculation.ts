cfe4844351875f5f229b5022f082f81cdcece846
Lawrence Brooks <84.lbrooks@gmail.com>
2024-10-17 10:51:25 -0400

    fix: handle hex colors without a preceeding # and refactor
    * function would return NaN in the matrix if supplied with a hex color
      without a preceeding #
    * eliminated double calls to parseInt when supplying a hex color
    * added handling for the case where supplied string is neither a hex nor
      rgb color (defaulting to black)
    * added tests
    
    Signed-off-by: Lawrence Brooks <84.lbrooks@gmail.com>

--- src/renderer/utils/colorMatrixCalculation.ts
+++ src/renderer/utils/colorMatrixCalculation.ts
@@ -1,48 +1,33 @@
 /* eslint-disable no-bitwise */
 export default function colorMatrixCalc(color: string, opacity: number) {
-  // Functions to convert colors
-  function hexToRgb(hex: string) {
-    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result
-      ? {
-          r: parseInt(result[1], 16),
-          g: parseInt(result[2], 16),
-          b: parseInt(result[3], 16),
-        }
-      : null;
-  }
-  // alert(hexToRgb("#0033ff").g); // "51";
+  function parseColor(color: string): { r: number, g: number, b: number } {
+    const hex_regex = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i;
+    const rgb_regex = /^rgb\(([\d]+), ([\d]+), ([\d]+)\)$/i;
 
-  // alert(rgbToHex(0, 51, 255)); // #0033ff
-  // function rgbToHex(r, g, b) {
-  //   return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
-  // }
-  // alert(rgbToHex(0, 51, 255)); // #0033ff
+    if (hex_regex.test(color)) {
+      const result = hex_regex.exec(color);
+      return {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      };
+    }
+    if (rgb_regex.test(color)) {
+      const result = rgb_regex.exec(color);
+      return {
+        r: parseInt(result[1]),
+        g: parseInt(result[2]),
+        b: parseInt(result[3]),
+      };
+    }
 
-  const regexTestHEX = /^#([0-9a-f]{3}){1,2}$/i;
-
-  // Calc matrix color but you need to convert to RGB color first
-  const calMatrix = (rgbColor: string, calOpacity: number) => {
-    const localRgbColor = rgbColor.replace(/[^\d,]/g, "").split(",");
-    const matrixColorInternal = `0 0 0 0 ${(parseInt(localRgbColor[0], 10) / 255).toFixed(2)} 0 0 0 0 ${(
-      parseInt(localRgbColor[1], 10) / 255
-    ).toFixed(2)} 0 0 0 0 ${(parseInt(localRgbColor[2], 10) / 255).toFixed(2)} 0 0 0 ${calOpacity} 0`;
-    return matrixColorInternal;
-  };
-
-  let safeRGBColor;
-  let matrixColor;
-
-  if (regexTestHEX.test(color)) {
-    // is Hexa
-    // console.log("Color is Hexa: ", color);
-    safeRGBColor = `rgb(${hexToRgb(color).r}, ${hexToRgb(color).g}, ${hexToRgb(color).b})`;
-    matrixColor = calMatrix(safeRGBColor, opacity);
-  } else {
-    // is rgba
-    // console.log("Color is RGB: ", color);
-    matrixColor = calMatrix(color, opacity);
+    // Supplied color could not be parsed, treat as black
+    return { r: 0, g: 0, b: 0 };
   }
 
-  return matrixColor;
+  const parsedColor = parseColor(color);
+  return `0 0 0 0 ${(parsedColor.r / 255).toFixed(2)} ` +
+    `0 0 0 0 ${(parsedColor.g / 255).toFixed(2)} ` +
+    `0 0 0 0 ${(parsedColor.b / 255).toFixed(2)} ` +
+    `0 0 0 ${opacity} 0`;
 }
