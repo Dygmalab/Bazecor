d9398c38bca8fc372532cb314bd6829b2b0e6bb9
Lawrence Brooks <84.lbrooks@gmail.com>
2024-10-17 12:54:26 -0400

    fix: opacity was no longer 0 in the invalid input case
    Signed-off-by: Lawrence Brooks <84.lbrooks@gmail.com>

--- src/renderer/utils/colorMatricCalculation.test.ts
+++ src/renderer/utils/colorMatricCalculation.test.ts
@@ -6,7 +6,7 @@ test.each([
   { color: "aABbcC", opacity: 1, expected: "0 0 0 0 0.67 0 0 0 0 0.73 0 0 0 0 0.80 0 0 0 1 0" },
   { color: "rGb(170, 187, 204)", opacity: 1, expected: "0 0 0 0 0.67 0 0 0 0 0.73 0 0 0 0 0.80 0 0 0 1 0" },
   { color: "rGb(255, 255, 0)", opacity: 0.65, expected: "0 0 0 0 1.00 0 0 0 0 1.00 0 0 0 0 0.00 0 0 0 0.65 0" },
-  { color: "qwerty", opacity: 1, expected: "0 0 0 0 0.00 0 0 0 0 0.00 0 0 0 0 0.00 0 0 0 0 0" },
+  { color: "qwerty", opacity: 0, expected: "0 0 0 0 0.00 0 0 0 0 0.00 0 0 0 0 0.00 0 0 0 0 0" },
 ])("colorMatrixCalc($color, $opacity) -> $expected", ({ color, opacity, expected }) => {
   expect(colorMatrixCalc(color, opacity)).toBe(expected);
 });
