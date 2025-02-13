46ec0415f5262c1191b2cf0af4aae93516f4d34d
Lawrence Brooks <84.lbrooks@gmail.com>
2025-02-13 09:54:59 -0500

    nit: Add comment indicating limited use of the file
    Signed-off-by: Lawrence Brooks <84.lbrooks@gmail.com>

--- src/api/focus/Focus.ts
+++ src/api/focus/Focus.ts
@@ -17,6 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+// NOTE: This file is only used to flash the OG Raise
 import log from "electron-log/renderer";
 import { spawn } from "child_process";
 import type { SerialPort, SerialPortOpenOptions } from "serialport";
